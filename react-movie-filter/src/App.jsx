32e9a7f8d634e578832410a1e4aea7af661af6b6
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-10-01 17:02:10 +0200

    first commit

---
+++ react-movie-filter/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FilmList from './assets/FilmList'
+
+function App() {
+  
+
+  return (
+    <>
+      <FilmList />
+    </>
+  )
+}
+
+export default App
---
+++ react-movie-filter/src/assets/FilmList.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+
+function FilmList() {
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ];
+
+  const [searchGenre, setSearchGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  useEffect(() => {
+    const filtered = movies.filter(movie =>
+      movie.genre.toLowerCase().includes(searchGenre.toLowerCase())
+    );
+    setFilteredMovies(filtered);
+  }, [searchGenre]);
+
+  return (
+    <div className="film-container">
+      <input
+        type="text"
+        placeholder="Cerca per genere"
+        value={searchGenre}
+        onChange={(e) => setSearchGenre(e.target.value)}
+      />
+
+      <ul>
+        {filteredMovies.length > 0 ? (
+          filteredMovies.map((movie, i) => (
+            <li key={i}>
+              {movie.title} ({movie.genre})
+            </li>
+          ))
+        ) : (
+          <li>Nessun film trovato</li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default FilmList;
+
